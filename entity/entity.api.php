d57ffb79fc8a568c5943917f1bf23ee8e7727702
fago <fago>
2010-01-11 17:06:17 +0000

    make entities optionally exportable by porting parts of ctools exportables to the entity API

--- entity/entity.api.php
+++ entity/entity.api.php
@@ -19,38 +19,34 @@
  *   the EntityDB class provided by the entity API or a custom class which
  *   extends EntityDB is suggested.
  * - exportable: Whether the entity is exportable. Defaults to FALSE.
+ * - 'object keys' - 'name': An optional name of a property that contains a
+ *   unique name of the entity. If specified, this is used as uniform identifier
+ *   of the entity while the 'id' key is only used to refer to the entity
+ *   internally, e.g. in the database. If not specified, this defaults to the
+ *   'id' key.
+ *   However for exportable entities, it's suggested to use a string here as
+ *   strings as names are more portable across systems. It's possible to go with
+ *   the numeric 'id', but be aware that export collisions are very likely.
  * - export: An array of optional information used for exporting. Known keys
  *   are:
- *   - key: The key used to identify the exported entity. By the default the
- *     entity identifier is used, but it's overridden by this setting.
- *     It's suggested to use a string here as string as names are more portable
- *     across systems. It is possible to use numbers, but be aware that export
- *     collisions are very likely.
- *   - can disable: Control whether or not the exportable objects can be
- *     disabled. All this does is cause the 'disabled' field on the object
- *     to always be set appropriately, and a variable is kept to record
- *     the state. Changes made to this state must be handled by the owner
- *     of the object. Defaults to TRUE.
- *   - status: Exportable objects can be enabled or disabled, and this status
- *     is stored in a variable. This defines what variable that is. Defaults
- *     to: 'default_' . $table,
  *   - default hook: What hook to invoke to find exportable objects that are
  *     currently defined. These will all be gathered into a giant array.
- *     Defaults to 'default_' . $table,
- *   - identifier: When exporting the object, the identifier is the variable that
- *     the exported object will be placed in. Defaults to $table.
+ *     Defaults to 'default_' . $entity_type.
+ *   - status key: The name of the entity property to use for setting the
+ *     entities status using defined bit flags. Defaults to 'status'.
+ *   - identifier: When exporting the object, the identifier is the variable
+ *     that the exported object will be placed in. Defaults to the entity type.
  *   - bulk export': Declares whether or not the exportable will be available
- *     for bulk exporting. (Bulk export UI is currently left to be handled
- *     by contrib, though the core hooks are present so this can be done.).
+ *     for bulk exporting.
  *   - export callback: The callback to use for bulk exporting. Defaults to
- *     $module . '_export_' . $table.
+ *     $module . '_export_' . $entity_type.
  *   - list callback: Bulk export callback to provide a list of exportable
  *     objects to be chosen for bulk exporting. Defaults to
- *     $module . '_' . $table . '_list'.
+ *     $module . '_' . $entity_type . '_list'.
  *   - to hook code callback: Function used to generate an export for the bulk
  *     export process. This is only necessary if the export is more complicated
- *     than simply listing the fields. Defaults to $module . '_' . $table .
- *     '_to_hook_code'.
+ *     than simply listing the fields. Defaults to $module . '_' .
+ *     $entity_type . '_to_hook_code'.
  */
 function entity_hook_entity_info() {
   $return = array(
@@ -62,7 +58,8 @@ function entity_hook_entity_info() {
       'fieldable' => TRUE,
       'object keys' => array(
         'id' => 'pid',
-        'bundle' => 'name',
+        'name' => 'name',
+        'bundle' => 'type',
       ),
       'bundles' => array(),
     ),
