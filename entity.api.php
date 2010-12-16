e3748614a594e94eed6377451e91e662ee3ac0c6
fago <fago>
2010-12-16 15:18:21 +0000

    #978832 improved the docs of hook_entity_enabled/disabled().

--- entity.api.php
+++ entity.api.php
@@ -328,37 +328,36 @@ function entity_hook_field_info() {
 }
 
 /**
- * Act when exportable entities are enabled.
+ * Respond when exportable entities are enabled.
  *
- * This hook is invoked for exportable entities regardless of their export
- * status as soon as new enabled entities are available to the system - either
- * as a new entity has been saved to the database or modules with entities in
- * code have been enabled.
- *
- * Note that there is no reliable way to react on configuration changes, as
- * entities in code may be updated anytime.
+ * This hook is invoked for entities as soon as new enabled entities are
+ * available to the system - either as a new entity has been inserted into the
+ * database or modules with entities in code have been enabled.
  *
  * @param $entities
  *   The entities keyed by entity ID.
  * @param $entity_type
  *   The type of entities being enabled (i.e. profile2_type, rules_config, ..).
+ *
+ * @see hook_entity_disabled()
  */
 function hook_entity_enabled($entities, $entity_type) {
   mymodule_initialize($entities, $entity_type);
 }
 
 /**
- * Act when exportable entities are disabled.
+ * Respond when exportable entities are disabled.
  *
- * This hook is invoked for exportable entities regardless of their export
- * status as soon as entities are unavailable to the system - either as an
- * customly created entity has been deleted from the database or modules with
- * entities in code have been disabled.
+ * This hook is invoked for entities when exportable entities have been disabled
+ * or disappeared - either as an customly created entity has been deleted from
+ * the database or modules providing default configurations have been disabled.
  *
  * @param $entities
  *   The entities keyed by entity ID.
  * @param $entity_type
  *   The type of entities being disabled (i.e. profile2_type, rules_config, ..).
+ *
+ * @see hook_entity_enabled()
  */
 function hook_entity_disabled($entities, $entity_type) {
   mymodule_deactivate($entities, $entity_type);
